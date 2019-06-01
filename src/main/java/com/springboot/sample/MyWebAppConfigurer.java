1cbca35d6be5815671035d38f66415a0b5e36b42
jack-huang2016 <2456120975@qq.com>
2019-06-01 15:18:15 +0800

    版本升级

--- src/main/java/com/springboot/sample/MyWebAppConfigurer.java
+++ src/main/java/com/springboot/sample/MyWebAppConfigurer.java
@@ -2,10 +2,10 @@ package com.springboot.sample;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
-public class MyWebAppConfigurer extends WebMvcConfigurerAdapter {
+public class MyWebAppConfigurer extends WebMvcConfigurationSupport {
 	  @Override
 	    public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		  	//相对路径
